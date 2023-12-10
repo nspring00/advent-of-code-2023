a7e5cb2be3ebf434173c418b0e04de067073edcc
Nikolaus Spring <nikolaus.spring@gmail.com>
2023-12-10 20:32:37 +0100

    Solve AoC 2023 day 10.2

--- src/10/main.zig
+++ src/10/main.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
-const utils = @import("../utils.zig");
-const iter_lines = utils.iter_lines;
 
 pub fn solve_1(inp: []const u8) u32 {
-
     const width = std.mem.indexOfScalar(u8, inp, '\n').? + 1;
     const start = std.mem.indexOfScalar(u8, inp, 'S').?;
 
@@ -14,12 +11,11 @@ pub fn solve_1(inp: []const u8) u32 {
     var last2 = start;
     var len: u32 = 1;
 
-    var upper = start - width;
+    const upper = start - width;
     if (inp[upper] == '|' or inp[upper] == 'F' or inp[upper] == '7') {
         pipe1 = upper;
-        last1 = upper;
     }
-    var lower = start + width;
+    const lower = start + width;
     if (inp[lower] == '|' or inp[lower] == 'J' or inp[lower] == 'L') {
         if (pipe1 == 0) {
             pipe1 = lower;
@@ -27,7 +23,7 @@ pub fn solve_1(inp: []const u8) u32 {
             pipe2 = lower;
         }
     }
-    var left = start - 1;
+    const left = start - 1;
     if (inp[left] == '-' or inp[left] == 'F' or inp[left] == 'L') {
         if (pipe1 == 0) {
             pipe1 = left;
@@ -35,7 +31,7 @@ pub fn solve_1(inp: []const u8) u32 {
             pipe2 = left;
         }
     }
-    var right = start + 1;
+    const right = start + 1;
     if (inp[right] == '-' or inp[right] == '7' or inp[right] == 'J') {
         if (pipe1 == 0) {
             pipe1 = right;
@@ -93,8 +89,98 @@ fn next_pipe(inp: []const u8, pipe: usize, last: usize, width: usize) usize {
 }
 
 pub fn solve_2(inp: []const u8) u64 {
-    _ = inp;
-    return 0;
+    const width = std.mem.indexOfScalar(u8, inp, '\n').? + 1;
+    const start = std.mem.indexOfScalar(u8, inp, 'S').?;
+    var current: usize = 0;
+    var last: usize = 0;
+    var prev = start;
+
+    // Find 2 adjacent pipes
+    const upper = start - width;
+    if (inp[upper] == '|' or inp[upper] == 'F' or inp[upper] == '7') {
+        current = upper;
+    }
+    const lower = start + width;
+    if (inp[lower] == '|' or inp[lower] == 'J' or inp[lower] == 'L') {
+        if (current == 0) {
+            current = lower;
+        } else {
+            last = lower;
+        }
+    }
+    const left = start - 1;
+    if (inp[left] == '-' or inp[left] == 'F' or inp[left] == 'L') {
+        if (current == 0) {
+            current = left;
+        } else {
+            last = left;
+        }
+    }
+    const right = start + 1;
+    if (inp[right] == '-' or inp[right] == '7' or inp[right] == 'J') {
+        if (current == 0) {
+            current = right;
+        } else {
+            last = right;
+        }
+    }
+
+    // Determine the pipe shape behind 'S'
+    var start_char: u8 = undefined;
+    if (current == upper and last == lower) {
+        start_char = '|';
+    } else if (current == upper and last == left) {
+        start_char = 'J';
+    } else if (current == upper and last == right) {
+        start_char = 'L';
+    } else if (current == lower and last == left) {
+        start_char = '7';
+    } else if (current == lower and last == right) {
+        start_char = 'F';
+    } else if (current == left and last == right) {
+        start_char = '-';
+    } else {
+        unreachable;
+    }
+
+    // Create a mask of all pipes that are part of the loop
+    var pipe_mask = std.mem.zeroes([20000]bool);
+    pipe_mask[start] = true;
+    pipe_mask[last] = true;
+    while (current != last) {
+        pipe_mask[current] = true;
+        const h = current;
+        current = next_pipe(inp, current, prev, width);
+        prev = h;
+    }
+
+    var inside = false;
+    var result: u32 = 0;
+    var start_horizontal_pipe: u8 = 0;
+
+    for (0..inp.len) |i| {
+        var c = inp[i];
+        if (i == start) {
+            c = start_char;
+        }
+
+        if (!pipe_mask[i] and inside) {
+            result += 1;
+        }
+
+        if (pipe_mask[i]) {
+            // Mark start of horizontal pipe
+            if (c == 'F' or c == 'L') {
+                start_horizontal_pipe = c;
+            }
+            // Needed to disinguish between cases L---J and L---7
+            if ((c == '7' and start_horizontal_pipe == 'L') or (c == 'J' and start_horizontal_pipe == 'F') or c == '|') {
+                inside = !inside;
+            }
+        }
+    }
+
+    return result;
 }
 
 const example_input = @embedFile("input-example.txt");
@@ -107,6 +193,9 @@ test "solve_1" {
 }
 
 test "solve_2" {
-    try std.testing.expectEqual(@as(u64, 0), solve_2(example_input));
-    try std.testing.expectEqual(@as(u64, 0), solve_2(input));
+    try std.testing.expectEqual(@as(u64, 1), solve_2(example_input));
+    try std.testing.expectEqual(@as(u64, 4), solve_2("...........\n.S-------7.\n.|F-----7|.\n.||.....||.\n.||.....||.\n.|L-7.F-J|.\n.|..|.|..|.\n.L--J.L--J.\n..........."));
+    try std.testing.expectEqual(@as(u64, 4), solve_2("..........\n.S------7.\n.|F----7|.\n.||....||.\n.||....||.\n.|L-7F-J|.\n.|II||II|.\n.L--JL--J.\n.........."));
+    try std.testing.expectEqual(@as(u64, 8), solve_2(".F----7F7F7F7F-7....\n.|F--7||||||||FJ....\n.||.FJ||||||||L7....\nFJL7L7LJLJ||LJ.L-7..\nL--J.L7...LJS7F-7L7.\n....F-J..F7FJ|L7L7L7\n....L7.F7||L7|.L7L7|\n.....|FJLJ|FJ|F7|.LJ\n....FJL-7.||.||||...\n....L---J.LJ.LJLJ..."));
+    try std.testing.expectEqual(@as(u64, 579), solve_2(input));
 }
