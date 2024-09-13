e7b9665175f1e6b64bfd146b8467987aaddcbccb
Nikolaus Spring <54682104+nspring00@users.noreply.github.com>
2024-09-13 13:54:07 +0200

    Solve AoC 2023 day 18.2

--- src/18/main.zig
+++ src/18/main.zig
@@ -4,8 +4,8 @@ const Allocator = std.mem.Allocator;
 const iter_lines = utils.iter_lines;
 
 const Point = struct {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 };
 
 pub fn solve_1(inp: []const u8, allocator: Allocator) u32 {
@@ -18,8 +18,6 @@ pub fn solve_1(inp: []const u8, allocator: Allocator) u32 {
     defer points.deinit();
 
     points.append(Point{ .x = 0, .y = 0 }) catch unreachable;
-
-    var boundary_len: i32 = 0;
 
     var lines_iter = iter_lines(inp);
     while (lines_iter.next()) |line| {
@@ -37,29 +35,57 @@ pub fn solve_1(inp: []const u8, allocator: Allocator) u32 {
         }
 
         points.append(new_point) catch unreachable;
-        boundary_len += distance;
     }
 
-    var area: i32 = 0;
+    return @truncate(compute_area(points.items));
+}
 
-    for (0..points.items.len) |i| {
-        const prev_point = points.items[(i + points.items.len - 1) % points.items.len];
-        const next_point = points.items[(i + 1) % points.items.len];
-        const point = points.items[i];
+fn compute_area(points: []Point) u64 {
+    var area: i64 = 0;
+
+    var boundary_len: u64 = 0;
+    for (0..points.len) |i| {
+        const prev_point = points[(i + points.len - 1) % points.len];
+        const next_point = points[(i + 1) % points.len];
+        const point = points[i];
+        boundary_len += @abs(point.x - prev_point.x) + @abs(point.y - prev_point.y);
         area += point.x * (prev_point.y - next_point.y);
     }
 
     const areaU = @abs(@divFloor(area, 2));
-    const i: u32 = areaU - @divFloor(@abs(boundary_len), 2) + 1;
 
-    return @abs(boundary_len) + i;
+    // Correct area for thickness of the boundary using Pick's theorem
+    return areaU + @divFloor(boundary_len, 2) + 1;
 }
 
-pub fn solve_2(inp: []const u8, allocator: Allocator) u32 {
-    _ = inp;
-    _ = allocator;
+pub fn solve_2(inp: []const u8, allocator: Allocator) u64 {
+    var points = std.ArrayList(Point).init(allocator);
+    defer points.deinit();
 
-    return 0;
+    points.append(Point{ .x = 0, .y = 0 }) catch unreachable;
+
+    var lines_iter = iter_lines(inp);
+    while (lines_iter.next()) |line| {
+        var line_iter = std.mem.split(u8, line, " ");
+        _ = line_iter.next() orelse unreachable;
+        _ = line_iter.next() orelse unreachable;
+        const color = line_iter.next() orelse unreachable;
+        const direction = color[7];
+        const distance = std.fmt.parseInt(i32, color[2..7], 16) catch unreachable;
+        const last_point = points.items[points.items.len - 1];
+        var new_point = Point{ .x = last_point.x, .y = last_point.y };
+        switch (direction) {
+            '0' => new_point.x += distance,
+            '2' => new_point.x -= distance,
+            '3' => new_point.y += distance,
+            '1' => new_point.y -= distance,
+            else => unreachable,
+        }
+
+        points.append(new_point) catch unreachable;
+    }
+
+    return compute_area(points.items);
 }
 
 const example_input = @embedFile("input-example.txt");
@@ -71,6 +97,6 @@ test "solve_1" {
 }
 
 test "solve_2" {
-    try std.testing.expectEqual(@as(u32, 0), solve_2(example_input, std.testing.allocator));
-    try std.testing.expectEqual(@as(u32, 0), solve_2(input, std.testing.allocator));
+    try std.testing.expectEqual(@as(u64, 952408144115), solve_2(example_input, std.testing.allocator));
+    try std.testing.expectEqual(@as(u64, 47452118468566), solve_2(input, std.testing.allocator));
 }
